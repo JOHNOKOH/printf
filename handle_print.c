8675ac22666bb5acf614ff9cffcf3356a8c430d4
JOHN OKOH <johnokoh735@gmail.com>
2023-06-29 09:57:42 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -2,11 +2,11 @@
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * @list: List of arguments to print.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: get width.
+ * @width: get the width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
